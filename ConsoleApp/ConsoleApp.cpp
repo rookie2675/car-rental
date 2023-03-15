48c63192c1f3e3811e0d4145f1fb7dc2d120b3ba
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-03-15 17:39:30 +0000

    created a class user and encapsulated the car cariables

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -4,22 +4,49 @@ using namespace std;
 #include <string>
 #include <regex>
 
-class Car 
-{
-    public:
-    string brand;
-    string model;
+class Car {
+    private:
+        string brand;
+        string model;
 
-    Car(string brand, string model)
-    {
-        this->brand = brand;
-        this->model = model;
-    }
+    public:
+        Car(string brand, string model)
+        {
+            this->brand = brand;
+            this->model = model;
+        }
+
+        string getBrand() {
+            return brand;
+        }
+};
+
+class User {
+    private:
+        string name;
+
+        void setName(string name) {
+            regex expression("/^[a-z ,.'-]+$/i");
+
+            if (!regex_match(name, expression))
+                throw invalid_argument("The following name is not valid: " + this->name);
+
+            this->name = name;
+        }
+
+    public:
+        User(string name) {
+            setName(name);
+        }
+
+        string getName() {
+            return name;
+        }
 };
 
 void printCars(Car cars[]) {
     for (int i = 0; i < 3; i++)
-        std::cout << i + 1 << ". " << cars[i].brand << "\n";
+        std::cout << i + 1 << ". " << cars[i].getBrand() << "\n";
 }
 
 int main()
@@ -27,10 +54,9 @@ int main()
     string name;
     cout << "Please enter your name: ";
     cin >> name;
-    regex expression("/^[a-z ,.'-]+$/i");
 
-    if (regex_match(name, expression))
-        cout << "The name is valid";
+    User user = User(name);
+    cout << "Welcome " << user.getName();
     
     Car cars[] = 
     { 
