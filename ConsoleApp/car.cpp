0452762fcd9d69de8209f7d307a77c95463919a1
roo-k13 <95150355+roo-k13@users.noreply.github.com>
2023-10-28 23:29:18 +0100

    Made the getter methods in Car class constants.

--- ConsoleApp/car.cpp
+++ ConsoleApp/car.cpp
@@ -1,14 +1,17 @@
 #include "car.h"
 
-Car::Car(std::string brand, std::string model) {
+Car::Car(std::string brand, std::string model) 
+{
     this->brand = brand;
     this->model = model;
 }
 
-std::string Car::getBrand() {
+std::string Car::getBrand() const 
+{
     return brand;
 }
 
-std::string Car::getModel() {
+std::string Car::getModel() const
+{
     return model;
 }
--- ConsoleApp/car.h
+++ ConsoleApp/car.h
@@ -3,15 +3,16 @@
 
 #include <string>
 
-class Car {
-private:
-    std::string brand;
-    std::string model;
+class Car 
+{
+    private:
+        std::string brand;
+        std::string model;
 
-public:
-    Car(std::string brand, std::string model);
-    std::string getBrand();
-    std::string getModel();
+    public:
+        Car(std::string brand, std::string model);
+        std::string getBrand() const;
+        std::string getModel() const;
 };
 
 #endif // CAR_H
